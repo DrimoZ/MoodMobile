79019b600f85ac12daac9b17c4afb35f9666e00c
XBanditxPandaX <108540082+XBanditxPandaX@users.noreply.github.com>
2023-12-09 16:07:23 +0100

    Create fragments for the different pages and the bottom navbar to throught all the pages

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.groupe5.moodmobile"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.groupe5.moodmobile"
@@ -32,6 +32,9 @@ android {
     }
     kotlinOptions {
         jvmTarget = "1.8"
+    }
+    buildFeatures {
+        viewBinding = true
     }
 }
 
--- app/src/main/java/com/groupe5/moodmobile/MainActivity.kt
+++ app/src/main/java/com/groupe5/moodmobile/MainActivity.kt
@@ -1,11 +1,91 @@
 package com.groupe5.moodmobile
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import com.groupe5.moodmobile.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setUpListeners()
+    }
+
+    override fun onStart() {
+        supportFragmentManager
+            .beginTransaction()
+            .add(
+                R.id.fragmentContainerView_mainActivity,
+                NewsFeedFragment.newInstance(),
+                "NewsFeedFragment"
+            )
+            .commit()
+        super.onStart()
+    }
+
+    private fun setUpListeners() {
+
+        binding.bottomNavigationView.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.navbar_newsFeed -> {
+                    supportFragmentManager
+                        .beginTransaction()
+                        .replace(
+                            R.id.fragmentContainerView_mainActivity,
+                            NewsFeedFragment.newInstance(),
+                            "NewsFeedFragment"
+                        )
+                        .commit()
+                }
+                R.id.navbar_messages -> {
+                    supportFragmentManager
+                        .beginTransaction()
+                        .replace(
+                            R.id.fragmentContainerView_mainActivity,
+                            MessageFragment.newInstance(),
+                            "MessageFragment"
+                        )
+                        .commit()
+                }
+                R.id.navbar_search -> {
+                    supportFragmentManager
+                        .beginTransaction()
+                        .replace(
+                            R.id.fragmentContainerView_mainActivity,
+                            SearchFragment.newInstance(),
+                            "SearchFragment"
+                        )
+                        .commit()
+                }
+                R.id.navbar_notification -> {
+                    supportFragmentManager
+                        .beginTransaction()
+                        .replace(
+                            R.id.fragmentContainerView_mainActivity,
+                            NotificationFragment.newInstance(),
+                            "NotificationFragment"
+                        )
+                        .commit()
+                }
+                R.id.navbar_profile -> {
+                    supportFragmentManager
+                        .beginTransaction()
+                        .replace(
+                            R.id.fragmentContainerView_mainActivity,
+                            ProfileFragment.newInstance(),
+                            "ProfileFragment"
+                        )
+                        .commit()
+                }
+            }
+            true
+        }
     }
 }
